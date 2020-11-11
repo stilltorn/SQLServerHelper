f538301d8a5fe61a62c3c3bef4fc0d47de91684a
stilltorn <am.stilltorn@gmail.com>
2020-11-11 17:59:44 +0200

    New functions, fixes to the first one

--- SQLServerHelper.psm1
+++ SQLServerHelper.psm1
@@ -1,16 +1,12 @@
 
-function Test-SqlConnection
+function Test-SqlInstanceConnection
 {
     # Returns $true if connection is successful
     Param(
         [Parameter(
             Mandatory=$true
         )]
-        [String]$Instance,
-        [Parameter(
-            Mandatory=$true
-        )]
-        [String]$Database,
+        [String]$Name,
         [Parameter(
             ParameterSetName='IntegratedAuth',
             Mandatory=$true
@@ -30,9 +26,9 @@ function Test-SqlConnection
     try
     {
         if ($IntegratedAuthentication.IsPresent) {
-            $connString = "Data Source=$Instance;Database=$Database;Integrated Security=True"
+            $connString = "Data Source=$Name;Integrated Security=True"
         } elseif ($Username -and $Password){
-            $connString = "Data Source=$Instance;Database=$Database;User ID=$UserName;Password=" + `
+            $connString = "Data Source=$Name;User ID=$UserName;Password=" + `
                 ($Password | ConvertFrom-SecureString -AsPlainText)
         }
 
@@ -47,13 +43,22 @@ function Test-SqlConnection
     }
     catch
     {
-        throw "Error connecting to $Instance\$($Database): $($_)"
+        throw "Error connecting to $($Instance): $($_.ToString())"
     }
 }
 
-function Get-SqlConnection
+# TODO
+# function Test-SqlDatabaseConnection
+# {
+# }
+# It's looking like it might make sense to just test connection to an 
+# instance and then list db's from said instance and see if the database
+# you're looking for is returned. Or something along those lines.
+
+function New-SqlDatabase
 {
-    # Returns a connection to a database
+    # NOTE Not sure yet what to ultimately return here so let's go with the DB for now
+    # NOTE Also not sure what to go with if the DB exists already so let's throw an error
     Param(
         [Parameter(
             Mandatory=$true,
@@ -93,17 +98,86 @@ function Get-SqlConnection
         try
         {
             if ($IntegratedAuthentication.IsPresent) {
-                $connString = "Data Source=$Instance;Database=$Database;Integrated Security=True"
+                $srv = New-Object Microsoft.SqlServer.Management.Smo.Server($Instance)
+                $srv.ConnectionContext.LoginSecure = $true
             } elseif ($Username -and $Password){
-                $connString = "Data Source=$Instance;Database=$Database;User ID=$UserName;Password=" + `
-                    ($Password | ConvertFrom-SecureString -AsPlainText)
+                $srv = New-Object Microsoft.SqlServer.Management.Smo.Server($Instance)
+                $srv.ConnectionContext.LoginSecure = $false
+                $srv.ConnectionContext.Login = $UserName
+                $srv.ConnectionContext.Password = ($Password | ConvertFrom-SecureString -AsPlainText)
             }
-
-            New-Object System.Data.SqlClient.SqlConnection $connString
         }
         catch
         {
-            throw "Error connecting to $Instance\$($Database): $($_)"
+            throw "Error connecting to $($Instance): $($_.ToString())"
+        }
+
+        try
+        {
+            if ($null -eq $srv.Databases[$Instance]) {
+                $db = New-Object Microsoft.SqlServer.Management.Smo.Database($srv, $Database)
+                $db.Create()
+                return $db
+            } else {
+                throw "Database $Instance\$($Database) already exists"
+            }
+        }
+        catch
+        {
+            throw "Error creating database $Instance\$($Database): $($_.ToString())"
         }
     }
 }
+
+function Get-SqlInstanceConnection
+{
+    # Returns a connection to a database
+    Param(
+        [Parameter(
+            Mandatory=$true,
+            Position=0,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [String]$Instance,
+        [Parameter(
+            ParameterSetName='IntegratedAuth',
+            Mandatory=$false,
+            Position=1,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [Switch]$IntegratedAuthentication,
+        [Parameter(
+            ParameterSetName='SimpleAuth',
+            Mandatory=$false,
+            Position=1,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [String]$UserName,
+        [Parameter(
+            ParameterSetName='SimpleAuth',
+            Mandatory=$false,
+            Position=2,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [SecureString]$Password
+    )
+    Process {
+        try
+        {
+            if ($IntegratedAuthentication.IsPresent) {
+                $connString = "Data Source=$Instance;Integrated Security=True"
+            } elseif ($Username -and $Password){
+                $connString = "Data Source=$Instance;User ID=$UserName;Password=" + `
+                    ($Password | ConvertFrom-SecureString -AsPlainText)
+            }
+
+            return New-Object System.Data.SqlClient.SqlConnection $connString
+        }
+        catch
+        {
+            throw "Error connecting to $($Instance): $($_.ToString())"
+        }
+    }
+}
+
+
