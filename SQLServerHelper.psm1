eab7031f09fb0996dd5ba348c21b322e75d620be
stilltorn <am.stilltorn@gmail.com>
2020-11-12 19:43:10 +0200

    Added Remove-SqlObject

--- SQLServerHelper.psm1
+++ SQLServerHelper.psm1
@@ -63,13 +63,27 @@ function Get-SqlColumnType
     Param(
         [Parameter(Mandatory=$true)][String]$ColumnType
     )
+    # NOTE I could not for the life of me figure out a better 
+    # way of declaring the type from a string variable
     if ($ColumnType -match 'NChar\(\d*?\)') {
         [Int]$i = ($ColumnType | Select-String -Pattern '\(\d*' ).Matches.Value -replace '\('
         $cType = [Microsoft.SqlServer.Management.Smo.DataType]::NChar($i)
     }
 
+    $typeMap = @{
+         TinyInt = [Microsoft.SqlServer.Management.Smo.DataType]::TinyInt
+        SmallInt = [Microsoft.SqlServer.Management.Smo.DataType]::SmallInt
+             Int = [Microsoft.SqlServer.Management.Smo.DataType]::Int
+          BigInt = [Microsoft.SqlServer.Management.Smo.DataType]::BigInt
+            Real = [Microsoft.SqlServer.Management.Smo.DataType]::Real
+        DateTime = [Microsoft.SqlServer.Management.Smo.DataType]::DateTime
+    }
+
     if (-not $cType) {
-        throw "Column type $ColumnType not recognized"
+        $cType = $typeMap.$ColumnType
+        if (-not $cType) {
+            throw "Column type $ColumnType not recognized"
+        }
     }
 
     return $cType
@@ -221,52 +235,49 @@ function New-SqlTable
     Param(
         [Parameter(
             Mandatory=$true,
-            Position=0,
             ValueFromPipelineByPropertyName=$true
         )]
         [String]$InstanceName,
         [Parameter(
             Mandatory=$true,
-            Position=1,
             ValueFromPipelineByPropertyName=$true
         )]
         [String]$DatabaseName,
         [Parameter(
             Mandatory=$true,
-            Position=2,
             ValueFromPipelineByPropertyName=$true
         )]
         [String]$TableName,
         [Parameter(
             Mandatory=$true,
-            Position=2,
             ValueFromPipelineByPropertyName=$true
         )]
         [String]$ColumnName, 
         [Parameter(
             Mandatory=$true,
-            Position=2,
             ValueFromPipelineByPropertyName=$true
         )]
-        [String]$ColumnType, # Such as NChar(50)
+        [String]$ColumnType,
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [Switch]$SetColumnAsIdentity,
         [Parameter(
             ParameterSetName='IntegratedAuth',
-            Mandatory=$false,
-            Position=3,
+            Mandatory=$true,
             ValueFromPipelineByPropertyName=$true
         )]
         [Switch]$IntegratedAuthentication,
         [Parameter(
             ParameterSetName='SimpleAuth',
-            Mandatory=$false,
-            Position=3,
+            Mandatory=$true,
             ValueFromPipelineByPropertyName=$true
         )]
         [String]$UserName,
         [Parameter(
             ParameterSetName='SimpleAuth',
-            Mandatory=$false,
-            Position=4,
+            Mandatory=$true,
             ValueFromPipelineByPropertyName=$true
         )]
         [SecureString]$Password
@@ -278,7 +289,7 @@ function New-SqlTable
             if ($IntegratedAuthentication.IsPresent) {
                 $server = New-Object Microsoft.SqlServer.Management.Smo.Server($InstanceName)
                 $server.ConnectionContext.LoginSecure = $true
-            } elseif ($Username -and $Password){
+            } elseif ($Username -and $Password) {
                 $server = New-Object Microsoft.SqlServer.Management.Smo.Server($InstanceName)
                 $server.ConnectionContext.LoginSecure = $false
                 $server.ConnectionContext.Login = $UserName
@@ -294,6 +305,13 @@ function New-SqlTable
             $database = $server.Databases[$DatabaseName]
             $table = New-Object Microsoft.SqlServer.Management.Smo.Table($database, $TableName)
             $column = New-Object Microsoft.SqlServer.Management.Smo.Column($table, $ColumnName, $cType)
+            if ($SetColumnAsIdentity.IsPresent -and $ColumnType -match 'Int') {
+                $column.Identity = $true
+                $column.IdentitySeed = 1
+                $column.IdentityIncrement = 1
+            } elseif ($SetColumnAsIdentity.IsPresent) {
+                throw "Only integer types supported as the identity column for now"
+            }
             $table.Columns.Add($column)
             $table.Create()
             return $table
@@ -312,52 +330,49 @@ function Add-SqlColumn
     Param(
         [Parameter(
             Mandatory=$true,
-            Position=0,
             ValueFromPipelineByPropertyName=$true
         )]
         [String]$InstanceName,
         [Parameter(
             Mandatory=$true,
-            Position=1,
             ValueFromPipelineByPropertyName=$true
         )]
         [String]$DatabaseName,
         [Parameter(
             Mandatory=$true,
-            Position=2,
             ValueFromPipelineByPropertyName=$true
         )]
         [String]$TableName,
         [Parameter(
             Mandatory=$true,
-            Position=2,
             ValueFromPipelineByPropertyName=$true
         )]
         [String]$ColumnName, 
         [Parameter(
             Mandatory=$true,
-            Position=2,
             ValueFromPipelineByPropertyName=$true
         )]
-        [String]$ColumnType, # Such as NChar(50)
+        [String]$ColumnType,
+        [Parameter(
+            Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [Switch]$SetColumnAsIdentity,
         [Parameter(
             ParameterSetName='IntegratedAuth',
-            Mandatory=$false,
-            Position=3,
+            Mandatory=$true,
             ValueFromPipelineByPropertyName=$true
         )]
         [Switch]$IntegratedAuthentication,
         [Parameter(
             ParameterSetName='SimpleAuth',
-            Mandatory=$false,
-            Position=3,
+            Mandatory=$true,
             ValueFromPipelineByPropertyName=$true
         )]
         [String]$UserName,
         [Parameter(
             ParameterSetName='SimpleAuth',
-            Mandatory=$false,
-            Position=4,
+            Mandatory=$true,
             ValueFromPipelineByPropertyName=$true
         )]
         [SecureString]$Password
@@ -384,6 +399,13 @@ function Add-SqlColumn
         {
             $table = $server.Databases[$DatabaseName].Tables[$TableName]
             $column = New-Object Microsoft.SqlServer.Management.Smo.Column($table, $ColumnName, $cType)
+            if ($SetColumnAsIdentity.IsPresent -and $ColumnType -match 'Int') {
+                $column.Identity = $true
+                $column.IdentitySeed = 1
+                $column.IdentityIncrement = 1
+            } elseif ($SetColumnAsIdentity.IsPresent) {
+                throw "Only integer types supported as the identity column for now"
+            }
             $table.Columns.Add($column)
             $table.Alter()
             return $table
@@ -392,6 +414,96 @@ function Add-SqlColumn
         {
             throw "Error adding column $ColumnName to database $InstanceName\$DatabaseName " + `
                 "table $($TableName): $($_.Exception.ToString())"
+        }
+    }
+}
+
+function Remove-SqlObject
+{
+    Param(
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [String]$InstanceName,
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [String]$DatabaseName,
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [String]$ObjectName,
+        [Parameter(
+            Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [String]$ColumnName,
+        [Parameter(
+            Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [String]$RowNumber, # TODO Not sure what to use here yet
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [ValidateSet('Database','Table','Column','Row')]
+        [String]$ObjectType,
+        [Parameter(
+            ParameterSetName='IntegratedAuth',
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [Switch]$IntegratedAuthentication,
+        [Parameter(
+            ParameterSetName='SimpleAuth',
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [String]$UserName,
+        [Parameter(
+            ParameterSetName='SimpleAuth',
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [SecureString]$Password
+    )
+    Process {
+        try
+        {
+            if ($IntegratedAuthentication.IsPresent) {
+                $server = New-Object Microsoft.SqlServer.Management.Smo.Server($InstanceName)
+                $server.ConnectionContext.LoginSecure = $true
+            } elseif ($Username -and $Password){
+                $server = New-Object Microsoft.SqlServer.Management.Smo.Server($InstanceName)
+                $server.ConnectionContext.LoginSecure = $false
+                $server.ConnectionContext.Login = $UserName
+                $server.ConnectionContext.Password = ($Password | ConvertFrom-SecureString -AsPlainText)
+            }
+        }
+        catch
+        {
+            throw "Error connecting to $($Instance): $($_.Exception.ToString())"
+        }
+        try
+        {
+            switch ($ObjectType) {
+                'Database' { throw "Database destuction not yet implemented" }
+                   'Table' {
+                                $table = $server.Databases[$DatabaseName].Tables[$ObjectName]
+                                $table.Drop()
+                           }
+                  'Column' { throw "Column destuction not yet implemented"  }
+                     'Row' { throw "Row destuction not yet implemented" }
+            }
+        }
+        catch
+        {
+            throw "Error removing $ObjectType $ObjectName from database $InstanceName\$($DatabaseName): " + `
+                "$($_.Exception.ToString())"
         }
     }
 }
